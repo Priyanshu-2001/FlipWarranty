650174dabde65b5150d119b5e6c9369cb0e7b420
Priyanshu-2001 <priyanshuhupta123123@gmail.com>
2022-07-14 12:26:08 +0530

    added fragments in buyer section

--- app/src/main/java/com/flip/warranty/customer/CustomerDashboard.kt
+++ app/src/main/java/com/flip/warranty/customer/CustomerDashboard.kt
@@ -7,6 +7,8 @@ import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.flip.warranty.MainActivity
 import com.flip.warranty.R
 import com.flip.warranty.databinding.ActivityCustomerDashboardBinding
@@ -28,15 +30,28 @@ class CustomerDashboard : AppCompatActivity() {
             interpolator = AccelerateDecelerateInterpolator()
         }
         binding.addBtn.setOnClickListener {
-            binding.addBtn.isVisible = false
-            binding.circleCover.isVisible = false
+//            binding.addBtn.isVisible = false
+//            binding.circleCover.isVisible = false
             binding.circleCover.startAnimation(animation) {
                 //display fragment when animation is finished
-
+                //TODO convert it into fragment
+                startActivity(Intent(this, GetWarranty::class.java))
 //                binding.Root.setBackgroundColor(ContextCompat.getColor(this, R.color.lightBrown))
                 binding.circleCover.isVisible = false
             }
 
+        }
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.purchaseFragment) {
+                binding.title.text = "Buy Now"
+            } else {
+                binding.title.text = "Profile"
+            }
         }
     }
 }
