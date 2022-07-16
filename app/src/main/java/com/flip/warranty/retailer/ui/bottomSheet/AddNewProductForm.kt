6fa13c951ea7a87df4c4d1bf8a2ee76bea586ead
Priyanshu-2001 <priyanshuhupta123123@gmail.com>
2022-07-16 23:25:49 +0530

    image picker added

--- app/src/main/java/com/flip/warranty/retailer/ui/bottomSheet/AddNewProductForm.kt
+++ app/src/main/java/com/flip/warranty/retailer/ui/bottomSheet/AddNewProductForm.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import com.flip.warranty.R
 import com.flip.warranty.databinding.AddNewProductBottomsheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -29,5 +30,21 @@ class AddNewProductForm : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         binding = AddNewProductBottomsheetBinding.bind(view)
 
+        binding.addImage.setOnClickListener {
+            imagePicLauncher.launch("image/*")
+        }
+        binding.deleteBtn.setOnClickListener {
+            binding.imageView.visibility = View.GONE
+            binding.addImage.visibility = View.VISIBLE
+            binding.deleteBtn.visibility = View.GONE
+        }
+
+    }
+
+    private val imagePicLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        binding.imageView.visibility = View.VISIBLE
+        binding.addImage.visibility = View.INVISIBLE
+        binding.deleteBtn.visibility = View.VISIBLE
+        binding.imageView.setImageURI(it)
     }
 }
